fefca9937c0dfbbc6e1accb898aa66bfbc423a59
tanszhe <1018595261@qq.com>
2019-04-08 16:26:32 +0800

    模板文件统一为小写

--- App/Exceptions/Handler.php
+++ App/Exceptions/Handler.php
@@ -14,9 +14,9 @@ class Handler
         if ($e->response->getHttpRequest()->isJson()) {
             return $e->response->json(format_json($e->getMessage(), $e->getCode(), $e->response->getHttpRequest()->id()));
         } else {
-            $file = _APP_PATH_VIEW_ . '/Exceptions/' . $e->getCode() . '.php';
+            $file = _APP_PATH_VIEW_ . '/exceptions/' . $e->getCode() . '.php';
             if (file_exists($file)) {
-                return $e->response->tpl('Exceptions/' . $e->getCode(), ['e' => $e]);
+                return $e->response->tpl('exceptions/' . $e->getCode(), ['e' => $e]);
             } else {
                 return $e->response->json(format_json($e->getMessage(), $e->getCode(), $e->response->getHttpRequest()->id()));
             }
