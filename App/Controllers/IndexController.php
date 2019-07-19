8f1b578c2cf8644247f322c7aeaf924472872c1d
tanszhe <1018595261@qq.com>
2019-07-19 17:15:37 +0800

    db

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controllers;
 
+use App\Model\User;
 use One\Http\Controller;
 
 class IndexController extends Controller
@@ -9,7 +10,27 @@ class IndexController extends Controller
 
     public function index()
     {
-        return 'hello world';
+        $res = User::find(1)->toArray();
+        print_r($res);
+
+        $res = User::where('id','>',3)->orderBy('id asc')->limit(2)->findAll()->toArray();
+        print_r($res);
+
+        $res = User::query("select * from users where id > 3 order by id asc limit 2")->toArray();
+        print_r($res);
+
+        $res = User::where('id','>',3)->count();
+        print_r($res);
+
+        echo "\n\n";
+        $res = User::where('id','>',3)->sum('age');
+        print_r($res);
+
+        $res = User::cache(5)->query("select * from users where id > 5 order by id asc limit 2")->toArray();
+        print_r($res);
+
+        $res = User::cache(5)->where('id','>',3)->orderBy('id asc')->limit(2)->findAll()->toArray();
+        print_r($res);
     }
 
     public function data(...$args)
--- App/Model/User.php
+++ App/Model/User.php
@@ -7,4 +7,23 @@ use One\Database\Mysql\Model;
 class User extends Model
 {
     CONST TABLE = 'users';
+
+    protected $_cache_time = 0;
+
+    public function events()
+    {
+        return [
+            'beforeGet' => function ($model, $sql) {
+//                echo $sql.PHP_EOL;
+                $this->whereOr('id', 6);
+//                $this->from('xxxx');
+//                $this->setConnection('read');
+                echo "\nbeforeGet\n";
+            },
+            'afterGet'  => function ($ret) {
+//                print_r($ret);
+                echo "\nafterGet\n";
+            }
+        ];
+    }
 }
