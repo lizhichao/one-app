1f5f084e1bd75b49b720c7fa285f97ded4d1f9c0
tanszhe <1018595261@qq.com>
2019-10-12 14:40:47 +0800

    shell

--- App/config.php
+++ App/config.php
@@ -12,10 +12,12 @@ if(isset($argv) && strpos($argv[0],'swoole.php') !== false){
     \App\Cloud\Server::setConfig(config('cloud', true));
     \One\Swoole\OneServer::setConfig(config(isset($argv[1]) ? $argv[1] : 'protocol', true));
     \One\Swoole\Client\Tcp::setConfig(config('client', true));
+
+    // 加载rpc配置
+    require _APP_PATH_ . '/Config/rpc.php';
 }
 
-// 加载rpc配置
-require _APP_PATH_ . '/Config/rpc.php';
+
 
 // 解析路由
 \One\Http\Router::loadRouter();
