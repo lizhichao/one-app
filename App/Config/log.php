45631a26158fcbc1a771a9c8ddbf104c3c10815f
tanszhe <1018595261@qq.com>
2020-03-10 17:54:45 +0800

    日志可配置

--- App/Config/log.php
+++ App/Config/log.php
@@ -1,7 +1,10 @@
 <?php
 
 return [
-    'path' => _APP_PATH_.'/RunCache/log', // 存放日志目录
-    'id' => uuid() // 每个请求的日志里的唯一id
+    'path'       => _APP_PATH_ . '/RunCache/log', // 存放日志目录
+    'id'         => uuid(), // 每个请求的日志里的唯一id
+//    'save_drive' => function ($code, $trace_id, $file, $line, $data) {
+//
+//    }
 
 ];
