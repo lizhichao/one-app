77799327de90a3afa5773f007ce0ac56ab0d1953
tanszhe <1018595261@qq.com>
2020-03-11 09:52:39 +0800

    sid

--- App/Config/log.php
+++ App/Config/log.php
@@ -3,7 +3,7 @@
 return [
     'path'       => _APP_PATH_ . '/RunCache/log', // 存放日志目录
     'id'         => uuid(), // 每个请求的日志里的唯一id
-//    'save_drive' => function ($code, $trace_id, $file, $line, $data) {
+//    'fn_save' => function ($code, $trace_id, $file, $line, $data) {
 //
 //    }
 
--- App/Config/session.php
+++ App/Config/session.php
@@ -2,7 +2,10 @@
 return [
     'drive' => 'file', //file,redis 保存session的驱动
     'name' => 'session_id', // session_id 名字 也就是发给客户端cookie的名字
-    'domain' => 'app.net'
+    'domain' => 'app.net',
+//    'fn_sid' => function ($response){
+//
+//    }
 ];
 
 
