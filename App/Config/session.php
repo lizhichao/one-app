5c65e91c08420374cb027299298a49bd97b7d175
tanszhe <1018595261@qq.com>
2020-04-02 16:33:02 +0800

    session

--- App/Config/session.php
+++ App/Config/session.php
@@ -1,4 +1,8 @@
 <?php
+
+// 打通 fpm 和 swoole 之间的session
+//ini_set('session.serialize_handler','php_serialize');
+
 return [
     'drive' => 'file', //file,redis 保存session的驱动
     'name' => 'session_id', // session_id 名字 也就是发给客户端cookie的名字
