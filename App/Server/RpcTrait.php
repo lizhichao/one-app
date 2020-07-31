13e276865dad5c4cc059dd8999079c6f8b6ab644
lzc <1018595261@qq.com>
2020-07-31 11:29:04 +0800

    log

--- App/Server/RpcTrait.php
+++ App/Server/RpcTrait.php
@@ -19,9 +19,8 @@ trait RpcTrait
         try {
             $arr = msgpack_unpack($data);
             if (isset($arr['c'])) {
-                $go_id = Log::setTraceId($arr['i'] . '.' . uuid());
+                Log::setTraceId($arr['i'] . '.' . uuid());
                 $str   = msgpack_pack(RpcServer::call($arr));
-                Log::flushTraceId($go_id);
             } else if ($ide === 1) {
                 $str = RpcServer::ideHelper($host, $px);
             } else {
@@ -29,9 +28,6 @@ trait RpcTrait
             }
             return $str;
         } catch (\Throwable $e) {
-            if(isset($go_id)){
-                Log::flushTraceId($go_id);
-            }
             error_report($e);
             return msgpack_pack([
                 'err' => $e->getCode(),
