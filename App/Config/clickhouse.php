3aa923f59a8318e78d1ebc36cbc258626793f3cf
lzc <1018595261@qq.com>
2020-09-24 11:57:07 +0800

    click

--- App/Config/clickhouse.php
+++ App/Config/clickhouse.php
@@ -1,14 +1,13 @@
 <?php
 
 return [
-    'debug_log' => true, // 是否打印sql日志
-    'default'      => [
+    'debug_log' => true,
+    'default'   => [
         'max_connect_count' => 10,
-        'host'        => env('ck.default.host', '127.0.0.1'),
-        'port'        => env('ck.default.port', 9000),
-        'database'    => env('ck.default.database', 'test'),
-        'user'        => env('ck.default.user', 'default'),
-        'passwd'      => env('ck.default.passwd', ''),
-        'compression' => env('ck.default.compression', false),
+        'host'              => env('ck.default.host', '127.0.0.1'),
+        'port'              => env('ck.default.port', 9000),
+        'database'          => env('ck.default.database', 'test'),
+        'user'              => env('ck.default.user', 'default'),
+        'password'          => env('ck.default.password', '')
     ]
 ];
