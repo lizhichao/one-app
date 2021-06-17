223b72bbc554d42f6d80f11ff1171dcd0445666c
tanszhe <1018595261@qq.com>
2021-06-17 15:58:53 +0800

    ct

--- App/Config/router.php
+++ App/Config/router.php
@@ -9,3 +9,10 @@ use One\Http\Router;
 Router::get('/', \App\Controllers\IndexController::class . '@index');
 
 Router::get('/data', \App\Controllers\IndexController::class . '@data');
+
+
+/**
+ * 创建模型 执行以下命令运行
+ * php shell.php shell/model
+ */
+Router::shell('/model', \App\Controllers\IndexController::class . '@model');
--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controllers;
 
+use One\Database\Mysql\ModelHelper;
 use One\Http\Controller;
 
 class IndexController extends Controller
@@ -15,6 +16,16 @@ class IndexController extends Controller
     public function data(...$args)
     {
         return $this->json($args);
+    }
+
+    public function model()
+    {
+        $m = new ModelHelper();
+        foreach ($m->getTables() as $t) {
+            $m->set($t)->createModel();
+            echo $t . PHP_EOL;
+        }
+
     }
 }
 
